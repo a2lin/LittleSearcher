41411e6308cd3c782b20b24cb7d1455fbe331600
Alexander Lin <lin.alexander.2011@gmail.com>
2013-11-16 23:32:58 -0800

    fixed tokenizer to tokenize & index based on lines (1 doc per line)

--- Tokenizer.py
+++ Tokenizer.py
@@ -12,17 +12,19 @@ class Tokenizer:
 			raise Exception("Please load with an actual filename")
 		self.fileDir = fileDir
 
-	def tokenize(self):
-		"""tokenizes the file generically. Returns the dict of word counts."""
+	def tokenize_index(self):
+		"""tokenizes the file generically. Returns the inverted index."""
 		inFile = open(self.fileDir, 'r')
+		i = 0
 		for line in inFile:
 			arr = re.split("[^A-Za-z']+", line)
 			for x in arr:
 				if(x != ''):
 					if x in self.dict:
-						self.dict[x] = self.dict[x] + 1
+						self.dict[x] = self.dict[x] + [i]
 					else:
-						self.dict[x] = 1
+						self.dict[x] = [i]
+			i += 1
 		#print self.dict
 		return self.dict
 
