5d136d3b7087fbfa621799815f4852844292b772
Alexander Lin <lin.alexander.2011@gmail.com>
2013-11-17 19:02:10 -0800

    Directory-Indexing Capability, QoL to SearchEngine/Tokenizer

--- SearchEngine.py
+++ SearchEngine.py
@@ -4,16 +4,21 @@ import Searcher
 class SearchEngine:
 	""" Main class for the Search Engine"""
 
-	def __init__(self):
-		self.index = {}
+	def __init__(self, index={}):
+		"""initializes the index & searcher"""
+
+		self.index = index
 		self.searcher = []
+		self.makeSearcher()
 
 	def makeSearcher(self):
 		""" Create a new Searcher to query"""
+
 		self.searcher += [Searcher.Searcher(self.index)]
 
 	def query(self, query):
 		""" Make sure that there are available searchers"""
+
 		if (len(self.searcher) == 0):
 			self.makeSearcher()
 		return self.searcher[0].search(query)
--- Tokenizer.py
+++ Tokenizer.py
@@ -8,12 +8,14 @@ class Tokenizer:
 
 	def load(self, fileDir=""):
 		"""loads the file into the tokenizer"""
+
 		if(fileDir == ""):
 			raise Exception("Please load with an actual filename")
 		self.fileDir = fileDir
 
-	def tokenize_index(self):
+	def tokenize_index(self, docId):
 		"""tokenizes the file generically. Returns the inverted index."""
+
 		inFile = open(self.fileDir, 'r')
 		i = 1
 		for line in inFile:
@@ -21,11 +23,14 @@ class Tokenizer:
 			for x in arr:
 				if(x != ''):
 					if x in self.dict:
-						self.dict[x] = self.dict[x] + [i]
+						self.dict[x] = self.dict[x] + [docId+str(i)]
 					else:
-						self.dict[x] = [i]
+						self.dict[x] = [docId+str(i)]
 			i += 1
 		#print self.dict
-		return self.dict
 
+	def get_index(self):
+		"""returns the current index held by this tokenizer"""
+
+		return self.dict
 
